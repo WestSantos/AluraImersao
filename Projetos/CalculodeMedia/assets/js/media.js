2f04c45d596ab5647d22f814cbe0ee588b331478
Weslley Teodoro dos Santos <94250413+WestSantos@users.noreply.github.com>
2022-03-08 21:47:54 -0300

    Add files via upload

---
+++ Projetos/CalculodeMedia/assets/js/media.js
@@ -0,0 +1,13 @@
+let nome ="Weslley"
+
+let notaDoPrimeiroBimestre = 10
+let notaDoSegundoBimestre = 5
+let notaDoTerceiroBimestre = 10
+let notaDoQuartoBimestre = 8
+
+let notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre)/4
+
+let notaArredondada = notaFinal.toFixed(1)
+
+console.log("Bem Vindo " + nome)
+console.log(notaArredondada)
