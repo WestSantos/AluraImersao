8e323026df26c33baaa46cd3c90da7d8f7ed5f27
Weslley Teodoro dos Santos <94250413+WestSantos@users.noreply.github.com>
2022-04-18 17:22:28 -0300

    Update main.js

--- Projetos/Mentalista/assets/js/main.js
+++ Projetos/Mentalista/assets/js/main.js
@@ -1,34 +1,55 @@
-var count= 0
+var numeroSecreto = parseInt(Math.random() * 11);
+var rodada = 1;
+var count = 0; 
+var acertos = 0;
+var erros = 0;
 
+function Chutar() {
+    count++;
 
-
-
-    function Chutar(){
-        var numeroSecreto = parseInt(Math.random() * 11);
-        count++
-        
-        var elementoResultado = document.getElementById("resultado");
-        var chute = parseInt(document.getElementById("valor").value);
-        
- 
-
-            if (chute == numeroSecreto) {
-                
-                elementoResultado.innerHTML = "Você acertou";
-                
-            } else if (chute > 10 || chute < 0){
-                elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10";
-        
-            }else if(count == 3){
-                elementoResultado.innerHTML = "Você errou 3 vezes o numero correto era "+numeroSecreto;
-            }else {
-                elementoResultado.innerHTML = "Errou, tente novamente";
-            };
-        console.log(numeroSecreto)
-        console.log(count)
-            
-        };
+    var elementoRodada = document.getElementById("rodadas")
+    var elementoAcertos = document.getElementById("acertos")
+    var elementoErros = document.getElementById("erros")
+    var elementoContador = document.getElementById("count")
+    var elementoResultado = document.getElementById("resultado");
+    var chute = parseInt(document.getElementById("valor").value);
     
+    console.log(chute);
 
- 
 
+    if (chute == numeroSecreto) {
+
+    elementoResultado.innerHTML = "Você acertou o numero secreto, jogue novamente";
+    numeroSecreto = parseInt(Math.random() * 11);
+    count = 0;
+    rodada++;
+    acertos++;
+    
+    } else if (chute > 10 || chute < 0) {
+
+        elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10";
+
+    }  else if(chute<numeroSecreto && count<3){
+
+        elementoResultado.innerHTML = "Você errou o numero secreto é maior"
+        erros++
+
+    } else if(chute>numeroSecreto && count<3){
+
+        elementoResultado.innerHTML = "Você errou o numero secreto é menor"
+        erros++
+
+    } else if(count == 3){
+
+        count = 0;
+        elementoResultado.innerHTML = "Você perdeu o numero secreto era " + numeroSecreto + " tente outra rodada"; 
+        numeroSecreto = parseInt(Math.random() * 11);
+        rodada++;
+        erros++
+
+    }
+    elementoRodada.innerHTML = rodada;
+    elementoAcertos.innerHTML = acertos;
+    elementoErros.innerHTML = erros;
+    elementoContador.innerHTML = count;
+}
